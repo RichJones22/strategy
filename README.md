c88ad6608c1097ca7a97a580cf5538be081696a0
Rich Jones <jones_rich@yahoo.com>
2015-06-24 12:43:37 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Welcome
+
+The **main.cpp** file in this repository illustrates how to implement the Strategy Design Pattern.
+
+This program was created using **Code::Blocks**, which can be found at **www.codeblocks.org**
+
+To use the program, just run the program.  If you are unfamiliar with Object Oriented polymorphic behavior, run the program via the debugger, placing a watch on "(Logger*)log".  In this way you can see, at runtime, how the Logger* type is transformed into one of the other subclass types.  
---
+++ main.cpp
@@ -0,0 +1,81 @@
+
+/**
+ * User: Rich
+ * Date: 6/22/2015
+ * Time: 9:12 AM
+ *
+ * Strategy Pattern:
+ * - A strategy pattern defines a family of algorithms.
+ * - Encapsulates each one.
+ * - And then makes them interchangeable.
+ *
+ * In this way we can build loosely coupled applications via polymorphism with the ability to swap out
+ * the various algorithms at runtime.
+ *
+ *
+ */
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+
+//Encapsulate and make them interchangeable
+class Logger {
+public:
+    virtual void log(string data)=0;
+};
+
+// Define a family of algorithms
+class LogToFile : public Logger{
+public:
+    void log(string data)
+    {
+        printf("Log %s to a file.\n", data.c_str());
+    }
+};
+
+class LogToDataBase : public Logger{
+public:
+    void log(string data)
+    {
+        printf("Log %s to a database.\n", data.c_str());
+    }
+};
+
+class LogToWebService : public Logger{
+public:
+    void log(string data)
+    {
+        printf("Log %s to a web service.\n", data.c_str());
+    }
+};
+
+// define an App class
+class App {
+public:
+    void log(string data, Logger *logger = nullptr)
+    {
+        logger = logger ?: new LogToFile;
+
+        logger->log(data);
+    }
+};
+
+int main()
+{
+    // create app instance
+    App *app = new App;
+
+    // pop. Logs for looping example.
+    void *Logs[] = {new LogToFile, new LogToDataBase, new LogToWebService, nullptr};
+
+    for(void *log : Logs)
+    {
+        app->log("Some DATA", (Logger*)log);
+    }
+    app->log("Some DATA");
+
+
+    return 0;
+}
